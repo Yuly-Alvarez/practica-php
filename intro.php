75c8d90f00cc8a024f30b5b35c665cf4a3f82243
Yuly <yulyalvarezb@gmail.com>
2026-05-27 21:08:48 -0500

    Primeras líneas del capítulo de PHP.

--- intro.php
+++ intro.php
@@ -6,27 +6,43 @@
     }
 </style>
 
-<!--EJEMPLO CON PHP-->
-<!--Opción 1-->
+
+<h1>EJEMPLO CON PHP</h1>
+<h2>Opción 1</h2>
 <?php
     echo "Hoy es " . date("l") . ". ";
 ?>
 Aquí están las noticias más recientes.
 <br>
 
-<!--Opción 2-->
+<h2>Opción 2</h2>
 Hoy es <?php echo date("l");?>. Aquí están las noticias más recientes. 
 <br> <br>
 
-<!--EJEMPLO CON JAVASCRIPT-->
-<!--Opción 1 (A menos que se necesite especificar una versión concreta de Javascript, se incluye "type: "text/javascript"")-->
+<h1>EJEMPLO CON JAVASCRIPT</h1>
+<h2>Opción 1 (A menos que se necesite especificar una versión concreta de Javascript, se incluye "type: "text/javascript"")</h2>
 <script type="text/javascript">
     document.write("Hoy es " + Date());
 </script>
 <br>
 
-<!--Opción 2-->
+<h2>Opción 2</h2>
 <script>
     document.write("Hoy es ", Date());
 </script>
+<br> <br>
 
+<h1>EJEMPLO "Hello world" CON ESTRUCTURA PHP</h1>
+ <?php 
+ echo "Hello world";
+
+    // Comentarios en PHP de una línea
+    /* Comentarios en
+    PHP multilínea */
+ ?>
+
+<h1>Sintaxis básica</h1>
+<?php 
+echo "Siempre finalizar la sentencia con ';'. Por ejemplo: $ x += 10; <br />";
+echo "Las variables inician con '$' (como el ejemplo anterior, pero sin el espacio).";
+ ?>
